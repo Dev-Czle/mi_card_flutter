e35228a628af6324cb50106a8c6ce08ec6aea8da
Dev-Czle <czle22@gmail.com>
2022-05-29 10:43:26 +0900

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
+// https://docs.flutter.dev/development/ui/widgets/layout
+
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Container(
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.fromLTRB(30, 10, 50, 20),
+            padding: EdgeInsets.all(30),
+            color: Colors.red.shade50,
+            child: Text('Hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
