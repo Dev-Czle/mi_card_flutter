b7ba234cea76b9ba18404c2496d3bf2b061f8479
Dev-Czle <czle22@gmail.com>
2022-05-29 13:28:03 +0900

    43번 테스트 코드 작성 완료

--- lib/main.dart
+++ lib/main.dart
@@ -13,29 +13,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Container(
                 width: 100,
-                height: 100,
+                height: double.infinity,
                 color: Colors.red.shade50,
                 child: Text('Container 1'),
               ),
-              SizedBox(
-                height: 20.0,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow.shade900,
+                  ),
+                ],
               ),
               Container(
                 width: 100,
-                height: 100,
-                color: Colors.amber,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100,
-                height: 100,
+                height: double.infinity,
                 color: Colors.red,
                 child: Text('Container 3'),
               ),
